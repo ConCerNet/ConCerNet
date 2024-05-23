6818d4bb9b9bb0cd8ddd0069fe81f7dbdef5f821
JJCO10 <juanjosecarmonaortiz@gmail.com>
2024-05-22 20:12:19 -0500

    transicion login

--- concernet/src/Pages/Login.jsx
+++ concernet/src/Pages/Login.jsx
@@ -36,40 +36,42 @@ const Login = () => {
         <>
             <div className="general">
                 <div className="error-message">{mensaje}</div>
-                    <form onSubmit={handleSubmitEvent}>
-                        <h1>ConCerNet</h1>
-                        <div className="form_control">
-                            <label htmlFor="user-name">Usuario:</label>
-                            <input
-                                type="text"
-                                id="user-name"
-                                name="username"
-                                placeholder="Nombre de usuario"
-                                aria-describedby="user-name"
-                                aria-invalid="false"
-                                onChange={handleInput}
-                            />
-                            <div id="user-name" className="sr-only">
-                                Por favor ingrese un nombre de usuario válido (mínimo 6 caracteres).
+                    <div className="login">
+                        <form onSubmit={handleSubmitEvent}>
+                            <h1>ConCerNet</h1>
+                            <div className="form_control">
+                                <label htmlFor="user-name">Usuario:</label>
+                                <input
+                                    type="text"
+                                    id="user-name"
+                                    name="username"
+                                    placeholder="Nombre de usuario"
+                                    aria-describedby="user-name"
+                                    aria-invalid="false"
+                                    onChange={handleInput}
+                                />
+                                <div id="user-name" className="sr-only">
+                                    Por favor ingrese un nombre de usuario válido (mínimo 6 caracteres).
+                                </div>
                             </div>
-                        </div>
-                        <div className="form_control">
-                            <label htmlFor="password">Contraseña:</label>
-                            <input
-                                type="password"
-                                id="password"
-                                name="password"
-                                placeholder="Contraseña"
-                                aria-describedby="user-password"
-                                aria-invalid="false"
-                                onChange={handleInput}
-                            />
-                            <div id="user-password" className="sr-only">
-                                Por favor ingrese una contraseña válida (mínimo 6 caracteres).
+                            <div className="form_control">
+                                <label htmlFor="password">Contraseña:</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    placeholder="Contraseña"
+                                    aria-describedby="user-password"
+                                    aria-invalid="false"
+                                    onChange={handleInput}
+                                />
+                                <div id="user-password" className="sr-only">
+                                    Por favor ingrese una contraseña válida (mínimo 6 caracteres).
+                                </div>
                             </div>
-                        </div>
-                        <button className="btn-submit">Iniciar sesión</button>
-                    </form>
+                            <button className="btn-submit">Iniciar sesión</button>
+                        </form>
+                    </div>
                 </div>
         </>
     );
