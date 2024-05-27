7b89f3d5509e548dd72361d067dde8ff3e09102e
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2024-05-27 11:29:15 -0500

    Modificacion de agendamiento de espacios

--- concernet/src/Pages/Agendamiento.jsx
+++ concernet/src/Pages/Agendamiento.jsx
@@ -1,21 +1,31 @@
+import React from "react";
 import NavBar from "../Components/NavBar";
-import piscina from '../Images/piscina.jpg';
-import cancha from '../Images/cancha.jpg';
-import salon from '../Images/salonSocial.jpeg';
+import '../Styles/Agendamiento.css';
+import { useParams } from "react-router-dom";
+import espacios from "../EspaciosData";
 
 const Agendamiento = () => {
+  const { id } = useParams();
+  const espacio = espacios.find((espacio) => espacio.id === parseInt(id));
+
+  if(!espacio){
+    return <div>El espacio no existe</div>
+  }
   return (
     <div className="agendamiento">
       <NavBar />
       <div className="contenedorAgendamiento">
-        <form action="#">
-            <label for="nombre">Nombre:</label>
+        <div className="agendaImagen">
+          <img src={espacio.imagen} alt={`Imagen del espacio ${espacio.nombre}`} />
+        </div>
+        <form className="calendario" action="#">
+            <label for="nombre"><b>Nombre:</b></label>
             <input type="text" id="nombre" name="nombre" required/>
 
-            <label for="fecha">Fecha:</label>
+            <label for="fecha"><b>Fecha:</b></label>
             <input type="date" id="fecha" name="fecha" required/>
 
-            <label for="hora">Hora:</label>
+            <label for="hora"><b>Hora:</b></label>
             <input type="time" id="hora" name="hora" required/>
 
             <button type="submit">Agendar</button>
@@ -26,3 +36,14 @@ const Agendamiento = () => {
 };
 
 export default Agendamiento;
+/*  const { id } = useParams();
+  const espacio = espacios.find((espacio) => espacio.id === parseInt(id));
+
+  if(!espacio){
+    return <div>El espacio no existe</div>
+  }
+
+
+<div className="agendaImagen">
+          <img src={espacio.imagen} alt={`Imagen del espacio ${espacio.nombre}`} />
+        </div> */
--- concernet/src/Pages/Espacios.jsx
+++ concernet/src/Pages/Espacios.jsx
@@ -4,27 +4,31 @@ import cancha from '../Images/cancha.jpg';
 import salon from '../Images/salonSocial.jpeg';
 import '../Styles/Espacios.css';
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import React from 'react';
 
 const Espacios = ()=>{
+    const espacios = [
+        { id: 1, imagen: piscina, nombre: 'piscina' },
+        { id: 2, imagen: cancha, nombre: 'cancha' },
+        { id: 3, imagen: salon, nombre: 'salon' },
+      ];
     return (
         <div className="espacio">
             <NavBar />
-            <div>
-                <div className="contenedorEspacios">
-                    <Link to="/Agendamiento"><img src={piscina} alt="Piscina" title="PISCINA"/></Link>
-                    <Link to="/Agendamiento"><img src={cancha} alt="Cancha" title="CANCHA"/></Link>
-                    <Link to="/Agendamiento"><img id="salon" src={salon} alt="Salon" title="SALON SOCIAL"/></Link>
-                </div>
-                <div className="pista">
-                    <p>Presiona el espacio que deseas agendar</p>
-                </div>
+            <div className="pista">
+                <h2>Presiona el espacio que deseas agendar</h2>
             </div>
+                <div className='espaciosList'>
+                    {espacios.map((espacio) => (
+                        <Link key={espacio.id} to={`/Espacios/${espacio.id}`}>
+                            <div className='contenedorEspacio'>
+                                <img id={espacio.nombre} src={espacio.imagen} alt={espacio.nombre} title={espacio.nombre} />
+                                <h3 className={espacio.nombre}>{espacio.nombre}</h3>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
         </div>
     );
 };
 export default Espacios;
-/*
-<label htmlFor="agenda">Agendar Espacio</label>
-<input type="date" id="agenda"/>
-*/
