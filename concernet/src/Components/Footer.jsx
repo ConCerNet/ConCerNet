f24704e01db9af05a2f410d373c32a49353fa073
JJCO10 <juanjosecarmonaortiz@gmail.com>
2024-05-27 18:12:55 -0500

    realizacion del footer

--- concernet/src/Components/Footer.jsx
+++ concernet/src/Components/Footer.jsx
@@ -1,44 +1,52 @@
 import "../Styles/Footer.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import youtube from "../Images/youtube.svg";
+import instagram from "../Images/instagram.svg";
+import whatsapp from "../Images/whatsapp.svg";
+import mail from "../Images/mail.svg";
+
 
 const Footer = () =>{
     return(
         <div className="footer">
             <footer className="footerContainer">
                 <ul className="social-icon">
-                    <li className="icon-img">
-                        <a href="">
-                            <img src="" alt="" />
+                    <li>
+                        <a href="" className="icon">
+                            <img src={youtube} alt="social1" />
                         </a>
-                        <a href="">
-                            <img src="" alt="" />
+                        <a href="" className="icon">
+                            <img src={instagram} alt="social2" />
                         </a>
-                        <a href="">
-                            <img src="" alt="" />
+                        <a href="" className="icon">
+                            <img src={whatsapp} alt="social3" />
                         </a>
-                        <a href="">
-                            <img src="" alt="" />
+                        <a href="" className="icon">
+                            <img src={mail} alt="social4" />
                         </a>
                     </li>
                 </ul>
                 <ul className="menu">
                     <li>
-                        <Link to="/Dashboard">Inicio</Link>
+                        <Link to="/Dashboard" className="enlace">Inicio</Link>
                     </li>
                     <li>
-                        <Link to="/Management">Administración</Link>
+                        <Link to="/Management" className="enlace">Administración</Link>
                     </li>
                     <li>
-                        <Link to="/Espacios">Espacios Comunes</Link>
+                        <Link to="/Espacios" className="enlace">Espacios Comunes</Link>
                     </li>
                     <li>
-                        <Link to="/Viviendas">Viviendas</Link>
+                        <Link to="/Viviendas" className="enlace">Viviendas</Link>
                     </li>
                     <li>
-                        <Link to="/Login">Iniciar sesión</Link>
+                        <Link to="/Login" className="enlace">Iniciar sesión</Link>
                     </li>
                 </ul>
+                <p className="text">
+                    2024 | Todos los derechos de autor reservados
+                </p>
             </footer>
         </div>
     );
